b51022e9d95c8443c3f71ad9bb2cea1fcf8dfbeb
Carlos <carlosfremdling@gmail.com>
2024-02-04 23:27:50 -0300

    first commit

---
+++ src/Cliente.java
@@ -0,0 +1,42 @@
+/**
+ * @deprecated
+ */
+public class Cliente {
+    private int codigo;
+    private String nome;
+    private String endereco;
+    private int cpf;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+}
+
